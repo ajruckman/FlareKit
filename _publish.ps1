f4070fd441a50a2b163d16798099f2ecf18dc34e
AJ Ruckman <aj.ruckman@mckee.com>
2019-10-17 14:01:24 -0400

    improve _publish.ps1 script

--- _publish.ps1
+++ _publish.ps1
@@ -1,9 +1,35 @@
+function Kill-ASPDotNETWebserver
+{
+    $webserver = $( Get-NetTCPConnection -LocalPort 5000 -ErrorAction Ignore )
+    if ($webserver)
+    {
+        Stop-Process -ErrorAction Ignore -Id $webserver.OwningProcess -Force
+    }
+}
+
+function Clean-DotNETProject
+{
+    Get-ChildItem .\ -include bin, obj -Recurse | foreach ($_) {
+        Write-Output $_.fullname
+        Remove-Item $_.fullname -Force -Recurse
+    }
+}
+
+Kill-ASPDotNETWebserver
 Clean-DotNETProject
+
 dotnet pack -c Debug
-Remove-Item -Force .\_published\*
+
+if (!(Test-Path .\_published\))
+{
+    md .\_published\
+}
+rm -Force .\_published\*
+
 cp .\FlareLib\bin\Debug\FlareLib.*.nupkg .\_published\
 cp .\FlareSelect\bin\Debug\FlareSelect.*.nupkg .\_published\
 cp .\FlareTables\bin\Debug\FlareTables.*.nupkg .\_published\
+
 Remove-Item -Force -Recurse -ErrorAction Ignore $HOME\.nuget\packages\flarelib\
 Remove-Item -Force -Recurse -ErrorAction Ignore $HOME\.nuget\packages\flareselect\
 Remove-Item -Force -Recurse -ErrorAction Ignore $HOME\.nuget\packages\flaretables\
