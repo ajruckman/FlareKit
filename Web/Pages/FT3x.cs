e775e2c89cecff71414aca03bd81b83d31a8c9ab
A.J. Ruckman <aj@ruckman.dev>
2020-04-08 18:19:09 -0400

    FlareSelect 3.4.3-alpha; FlareTables 3.6.4-alpha: Update dependencies

--- Web/Pages/FT3x.cs
+++ Web/Pages/FT3x.cs
@@ -65,7 +65,7 @@ namespace Web.Pages
             _flareTable1.RegisterColumn(nameof(Record.Age),     filterValue: "1",  width: "60px");
             _flareTable1.RegisterColumn(nameof(Record.City),    filterable: false, sortable: false);
             _flareTable1.RegisterColumn(nameof(Record.State),   sortable: false);
-            _flareTable1.RegisterColumn(nameof(Record.Country), shown: true,     filterable: false);
+            _flareTable1.RegisterColumn(nameof(Record.Country), shown: true,     filterable: false, displayName:"");
             _flareTable1.RegisterColumn(nameof(Record.Zip),     monospace: true, width: "120px");
 
             _flareTable1.OnRowClick += (Record record) => Console.WriteLine($"Click: {record.Name}");
