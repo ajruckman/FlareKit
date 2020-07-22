18836135378f6a75b41e1214ebf9f43cf0e55e0a
AJ Ruckman <aj.ruckman@mckee.com>
2020-07-22 10:36:01 -0400

    FlareSelect 3.7.2-alpha: Add Superset dependencies to ResourceSets.FlareSelect

--- FlareSelect/ResourceSets.cs
+++ FlareSelect/ResourceSets.cs
@@ -7,6 +7,11 @@ namespace FlareSelect
         public static readonly ResourceSet FlareSelect =
             new ResourceSet(nameof(FlareSelect), nameof(FlareSelect),
                 stylesheets: new[] {"FlareSelect.css"},
-                dependencies: new[] {ShapeSet.ResourceSets.ShapeSet});
+                dependencies: new[]
+                {
+                    Superset.Web.ResourceSets.FocusElement, 
+                    Superset.Web.ResourceSets.Listeners,
+                    ShapeSet.ResourceSets.ShapeSet, 
+                });
     }
 }
